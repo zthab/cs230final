72de150b5b75123b5e5cc72f82a918901c8e33c9
Nolen Belle <nbryant2@wellesley.edu>
2018-12-01 15:47:19 -0500

    Started Person Class

---
+++ Person.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Option here.
+ *
+ * @author (nbryant2,gbronzi,zthabet)
+ * @version (12.1.18)
+ */
+public class Person{
+    private int sleepScore, smartScore, socialScore;
+    
+}
