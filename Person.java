3a7502ad5804caa582e5c0d1a0ba26662eecb3e8
Nolen Belle <nbryant2@wellesley.edu>
2018-12-01 15:56:32 -0500

    some instance variables added to person

--- Person.java
+++ Person.java
@@ -7,13 +7,14 @@
  */
 public class Person
 {
+    int sleepScore, smartScore, socialScore;
+    private final static int[] wendy = {2, 3, 3};// archtypes
+    private final static int[] athletic = {2, 2, 4};
+    private final static int[] horse = {0, 0, 0};
+    private final static int[] hermit = {4, 5, 0};
+    private final static int[] society = {1, 2, 3};
+    private final static int[] offCampus = {0, 3, 4};
     
-    final static int[] wendy = {2, 3, 3};// archtypes
-    final static int[] athletic = {2, 2, 4};
-    final static int[] horse = {0, 0, 0};
-    final static int[] hermit = {4, 5, 0};
-    final static int[] society = {1, 2, 3};
-    final static int[] offCampus = {0, 3, 4};
 
 
     /**
