a72ce4d78abb50bc22fb7de1f70c29818f3fbf47
Giulia Bronzi <giuliabronzi15@gmail.com>
2018-12-09 13:22:25 -0500

    updated person class

--- Person.java
+++ Person.java
@@ -8,29 +8,58 @@
 public class Person
 {
     int sleepScore, smartScore, socialScore;
-    private final static int[] wendy = {2, 3, 3};// archtypes
-    private final static int[] athletic = {2, 2, 4};
-    private final static int[] horse = {0, 0, 0};
-    private final static int[] hermit = {4, 5, 0};
-    private final static int[] society = {1, 2, 3};
-    private final static int[] offCampus = {0, 3, 4};
+    int[] character;
     
-
-
+    private final static int[] athletic = {2, 2, 4};
+    private final static int[] hermit = {4, 5, 0};
+    private final static int[] horse = {0, 0, 0};
+    private final static int[] offCampus = {0, 3, 4};
+    private final static int[] society = {1, 2, 3};
+    private final static int[] wendy = {2, 3, 3};
+    
+    private final static int[][] characters = {athletic,hermit,horse,offCampus,society,wendy};
+    //was thinking we could do array of arrays for characters, through gui, each button for 
+    //the characters has a different number assigned, which will be the charInt inputted 
+    //to create the char
     /**
      * Constructor for objects of class Person
+     * 
+     * @param charInt 
      */
-    public Person()
+    public Person(int charInt)
     {
-        // initialise instance variables
+        character = characters[charInt];
+        sleepScore = character[0];
+        smartScore = character[1];
+        socialScore = character[2];
         
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Returns the sleep score
+     * 
+     * @return sleepScore
      */
+    public int getSleepScore(){
+        return sleepScore;
+    }
+    
+    /**
+     * Returns the smart score
+     * 
+     * @return smartScore
+     */
+    public int getSmartScore(){
+        return smartScore;
+    }
+    
+    /**
+     * Returns the social score
+     * 
+     * @return socialScore
+     */
+    public int getSocialScore(){
+        return socialScore;
+    }
     
 }
