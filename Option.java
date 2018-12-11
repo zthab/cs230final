d430eb235793f7dbfd8e88d8425b793ca2b6a197
Giulia Bronzi <giuliabronzi15@gmail.com>
2018-12-10 22:10:20 -0500

    updates to situation, option, and person classes, various methods and commenting

--- Option.java
+++ Option.java
@@ -7,15 +7,37 @@
  */
 public class Option
 {
-
+    private String option;
+    private int[] points;
+    
     /**
-     * Constructor for objects of class Option
+     * Constructor for objects of class Option, option and point are inputted
      */
-    public Option()
+    public Option(String opt, int[] pts)
     {
-        // initialise instance variables
-        
+        option = opt;
+        points = pts;
     }
 
+    public String getOption(){
+        return option;
+    }
+    
+    public int[] getPoints(){
+        return points;
+    }
+    
+    public void setOption(String o){
+        option = o;
+    }
+    
+    public void setPoints(int[] p){
+        points = p;
+    }
+    
+    public String toString(){
+        return "Option: " + option + "/nSleep: " + points[0] + "/nSmart: " + points[1] 
+                + "/nSocial: " + points[2]; 
+    }
 
 }
