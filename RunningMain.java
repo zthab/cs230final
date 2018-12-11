77f87f486896bf620b8f04a6a34eaa349fe02bc2
Zahra Thabet <zthabet2018@gmail.com>
2018-12-11 16:43:50 -0500

    fixed running mini game by consolidating the time and button pushing components into the same panel

--- RunningMain.java
+++ RunningMain.java
@@ -13,35 +13,35 @@ import javax.swing.* ;
 import java.awt.*;
 
 public class RunningMain extends javax.swing.JFrame {
-    private JSplitPane splitPanel;  // split the window in top and bottom
+    //private JSplitPane splitPanel;  // split the window in top and bottom
     private JPanel countPanel;
     private JPanel runPanel;
     
     public RunningMain(){
-        splitPanel = new JSplitPane();
+        //splitPanel = new JSplitPane();
         
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        //JPanel mainPanel = new JPanel();
+        //mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         
         JLabel timerField = new JLabel();
         
         
-        JPanel runPanel = new RunningPanel();
-        JPanel countPanel = new CountdownPanel(timerField);
-        countPanel.add(timerField);
+        //JPanel runPanel = new RunningPanel(timerField);
+        //JPanel countPanel = new CountdownPanel(timerField);
+        //countPanel.add(timerField);
         
         //getContentPane().setLayout(new GridLayout());
-        getContentPane().add(splitPanel);
+        //getContentPane().add(splitPanel);
        
         //Set the panel details
-        splitPanel.setOrientation(JSplitPane.VERTICAL_SPLIT);
-        splitPanel.setDividerLocation(160); 
-        splitPanel.setTopComponent(countPanel);
-        splitPanel.setBottomComponent(runPanel);
-        
+        //splitPanel.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        //splitPanel.setDividerLocation(160); 
+        //splitPanel.setTopComponent(countPanel);
+        //splitPanel.setBottomComponent(runPanel);
+        //mainPanel.add(new RunningPanel(timerField));
         //int time = countPanel.getCounter(); 
         //not sure how to get the state of the counter from CountdownPanel
-
+        add (new RunningPanel(timerField));
         setPreferredSize(new Dimension(400, 400));
         pack();       
     }
--- RunningPanel.java
+++ RunningPanel.java
@@ -12,89 +12,115 @@ import javax.swing.* ;
 import java.awt.Color;
 import java.util.Random;
 
-public class  RunningPanel extends JPanel
+public class  RunningPanel extends JPanel 
 {
-   private int count, time;
-   private JButton push;
-   private JLabel label;
-   private Timer timeClock;
-   
-   final int TOTAL_TIME = 20; //seconds to play the game
+    private int count, time;
+    private JButton push;
+    private JLabel label;
+    private Timer timeClock;
+
+    final int TOTAL_TIME = 20; //seconds to play the game
     int counter = TOTAL_TIME;
     javax.swing.Timer refreshTimer;
     //Creation of a JTextField
     JLabel countdownTimerField ;
 
-   public RunningPanel ()
-   {
-      count = 0; 
-      
-      // timeClock = new Timer(1000, new TimerListener () );
-      // timeClock.setActionCommand("Start running!");
-      // timeClock.setRepeats(true);
-      // timeClock.start();
+    private int finalCount;
 
-      push = new JButton ("Run!");
-      push.addActionListener (new ButtonListener() );
+    public RunningPanel (JLabel f)
+    {
+        count = 0; 
+        // timeClock = new Timer(1000, new TimerListener () );
+        // timeClock.setActionCommand("Start running!");
+        // timeClock.setRepeats(true);
+        // timeClock.start();
 
-      label = new JLabel ("Steps: " + count);
+        refreshTimer = new javax.swing.Timer(1000, new timeListener());
+        countdownTimerField = f;
+        refreshTimer.start();
+        
+        push = new JButton ("Run!");
+        push.addActionListener (new ButtonListener() );
 
-      add (push); // it meand this.add(push) b/c that's the default panel
-      add (label);
-      
-      push.setVisible(true);
+        label = new JLabel ("Steps: " + count);
+        add(countdownTimerField);
+        add (push); // it meand this.add(push) b/c that's the default panel
+        add (label);
 
-      setBackground (Color.cyan);
-      
-      //setPreferredSize (new Dimension(300, 40));
-   }
+        push.setVisible(true);
 
-   //*****************************************************************
-   //  Represents a listener for button push (action) events.
-   //*****************************************************************
-   private class ButtonListener implements ActionListener
-   {
-      //--------------------------------------------------------------
-      //  Updates the counter and label when the button is pushed.
-      //--------------------------------------------------------------
-      public void actionPerformed (ActionEvent event)
-      {
-         count++; // When ANY button is clicked! (If there were more...)
-         label.setText("Steps: " + count);
-         Random rand = new Random();
-         int r = rand.nextInt(9);
-         
-         if (r==1)
-            setBackground (Color.green);
-            
-         if (r==2)
-            setBackground (Color.magenta);
-            
-         if (r==3)
-            setBackground (Color.yellow);
-            
-         if (r==4)
-            setBackground (Color.blue);   
-            
-         if (r==5)
-            setBackground (Color.black);
-            
-         if (r==6)
-            setBackground (Color.yellow);
-            
-         if (r==7)
-            setBackground (Color.lightGray);
-            
-         if (r==8)
-            setBackground (Color.blue);
-            
-         if (r==9)
-            setBackground (Color.orange);
-            
-         if (r==0)
-            setBackground (Color.cyan);
-      }
-   }
-   
+        setBackground (Color.cyan);
+
+        //setPreferredSize (new Dimension(300, 40));
+    }
+
+    //*****************************************************************
+    //  Represents a listener for button push (action) events.
+    //*****************************************************************
+    private class timeListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            counter--;
+            if (counter >= 0){
+                //System.out.println(" ** " + counter);
+                countdownTimerField.setText(" Time left: " + counter);
+            }
+            if (counter == 0){
+                //System.out.println("Time's UP! ");
+                countdownTimerField.setText(" Time's UP!");
+                refreshTimer.stop();
+                finalCount=count;
+                System.out.println(finalCount);
+                remove(push);
+                setBackground(Color.white);
+                label.setText("Final Step Count: "+finalCount);
+            }
+        }
+    }
+    private class ButtonListener implements ActionListener
+    {
+        //--------------------------------------------------------------
+        //  Updates the counter and label when the button is pushed.
+        //--------------------------------------------------------------
+        public void actionPerformed (ActionEvent event)
+        {
+
+            count++; // When ANY button is clicked! (If there were more...)
+            label.setText("Steps: " + count);
+            Random rand = new Random();
+            int r = rand.nextInt(9);
+
+            if (r==1)
+                setBackground (Color.green);
+
+            if (r==2)
+                setBackground (Color.magenta);
+
+            if (r==3)
+                setBackground (Color.yellow);
+
+            if (r==4)
+                setBackground (Color.blue);   
+
+            if (r==5)
+                setBackground (Color.pink);
+
+            if (r==6)
+                setBackground (Color.yellow);
+
+            if (r==7)
+                setBackground (Color.lightGray);
+
+            if (r==8)
+                setBackground (Color.blue);
+
+            if (r==9)
+                setBackground (Color.orange);
+
+            if (r==0)
+                setBackground (Color.cyan);
+
+        }
+    }
+
 }
 
