0d340de488f9e92e3271ff7a9411688bcd458008
Giulia Bronzi <giuliabronzi15@gmail.com>
2018-12-13 12:42:19 -0500

    added the javaFoundations

--- InstructionsPanel.java
+++ InstructionsPanel.java
@@ -4,8 +4,8 @@ import java.awt.event.*;
 /**
  * Write a description of class InstructionsPanel here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (nbryant2, zthabet, gbronzi)
+ * @version (12.1.18)
  */
 public class InstructionsPanel extends JPanel 
 {
@@ -15,7 +15,16 @@ public class InstructionsPanel extends JPanel
     public InstructionsPanel()
     {
        setBackground (Color.green);
-       JTextArea l1 = new JTextArea ("put in instructions");
+       JTextArea l1 = new JTextArea ("Welcome to The Wellesley Trail! The goal of this game is to" +
+                                     " make it to graduation without getting lost in the tunnels," + 
+                                     " attacked by geese, or dying from sleep deprivation.\n" +
+                                     " First you will pick from six different archtypes; each have" +
+                                     " a different set of points for the sleep, smart and social" +
+                                     " categories. You will then be prompted with questions and will" +
+                                     " have two options to pick from. Based on some decisions, you" + 
+                                     " will have to play a mini-game. If you loose this mini-game," +
+                                     " you loose everything. You will also gain or loose points in" +
+                                     " the three categories based on the decisions you choose. ");
                                     
        l1.setBackground(Color.green); //so that the JTextArea matches the panel background
        add (l1);
