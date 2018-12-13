4b0956e2441217881f747540337b1514d4e9fc84
Giulia Bronzi <giuliabronzi15@gmail.com>
2018-12-13 13:25:27 -0500

    updates to instruction and graduation panel

--- GraduationPanel.java
+++ GraduationPanel.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.io.*;
 /**
  * Write a description of class GraduationPanel here.
  *
@@ -16,10 +19,22 @@ public class GraduationPanel extends JPanel
     public GraduationPanel()
     {
         setBackground (Color.green);
-       JTextArea l1 = new JTextArea ("graduation note");
+       JTextArea l1 = new JTextArea ("Congratulations, you made it to graduation! You succesfully " +
+                                     "passed all your classes while balacing friends, orgs, and " +
+                                     "all the terrors Wellesley has to offer. Here is your diploma " +
+                                     "and good luck dealing with the real world!");
                                     
        l1.setBackground(Color.green); //so that the JTextArea matches the panel background
        add (l1);
+       
+       try{
+            BufferedImage diploma = ImageIO.read(new File("diploma.png"));
+            JLabel picLabel = new JLabel(new ImageIcon(diploma));
+            add(picLabel);
+        }   catch(IOException e){
+            System.out.println("Image not found in directory.");
+        }
+       
     }
 
 }
--- InstructionsPanel.java
+++ InstructionsPanel.java
@@ -17,17 +17,33 @@ public class InstructionsPanel extends JPanel
        setBackground (Color.green);
        JTextArea l1 = new JTextArea ("Welcome to The Wellesley Trail! The goal of this game is to" +
                                      " make it to graduation without getting lost in the tunnels," + 
-                                     " attacked by geese, or dying from sleep deprivation.\n" +
-                                     " First you will pick from six different archtypes; each have" +
+                                     " attacked by geese, or dying from sleep deprivation.");
+                                     
+       JTextArea l2 = new JTextArea (" First you will pick from six different archtypes; each have" +
                                      " a different set of points for the sleep, smart and social" +
                                      " categories. You will then be prompted with questions and will" +
-                                     " have two options to pick from. Based on some decisions, you" + 
-                                     " will have to play a mini-game. If you loose this mini-game," +
-                                     " you loose everything. You will also gain or loose points in" +
-                                     " the three categories based on the decisions you choose. ");
+                                     " have two options to pick from.");
+       JTextArea l3 = new JTextArea  (" Based on some decisions, you will have to play a mini-game." +
+                                      " If you loose this mini-game,you loose everything." );
+                                      
+       JTextArea l4 = new JTextArea  ("You will also gain or loose points in the three categories" +
+                                      " based on the decisions you choose. If " +
+                                      " any of these categories drop below zero, you loose, so " +
+                                      " choose wisely and good luck!!");
                                     
        l1.setBackground(Color.green); //so that the JTextArea matches the panel background
        add (l1);
        
+    }
+    
+    public void actionPerformed (ActionEvent event){
+        JButton button = (JButton)event.getSource();
+        JPanel buttonPanel = (JPanel)button.getParent();
+        JPanel cardLayoutPanel = (JPanel)buttonPanel.getParent();
+        //make new situation panel from source
+        CardLayout layout = (CardLayout)cardLayoutPanel.getLayout();
+        CharSelection nextPanel = new CharSelection(new Person(0)); 
+        cardLayoutPanel.add(nextPanel,"3");
+        layout.show(cardLayoutPanel, "3");
     }
 }
