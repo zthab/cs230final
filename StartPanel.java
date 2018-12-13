c61c89a5acb7042fb28a2ca65c7a7e73c757d48a
Zahra Thabet <zthabet2018@gmail.com>
2018-12-13 13:13:24 -0500

    Made TrailGui cleaner, linked start panel to instructions panel

--- StartPanel.java
+++ StartPanel.java
@@ -17,11 +17,38 @@ public class StartPanel extends JPanel
     public StartPanel()
     {
        setBackground (Color.green);
-       JTextArea l1 = new JTextArea ("Welcome to Wellesley Trails!");
-                                    
+       setLayout(new BorderLayout());
+       JLabel l1 = new JLabel ("Welcome to Wellesley Trails!");
+       l1.setHorizontalAlignment(SwingConstants.CENTER);
+                             
+       JButton next = new JButton("Next");
+       next.addActionListener(new ButtonListener());
+
+        //option1Button.addActionListener(new ButtonListener());
+        
        l1.setBackground(Color.green); //so that the JTextArea matches the panel background
-       add (l1);
+        add(next,BorderLayout.SOUTH);
+       add (l1, BorderLayout.CENTER);
+      
        
     }
+    private class ButtonListener implements ActionListener
+    {
+        //-----------------------------------------------------------------
+        // Sets the text of the label depending on which radio
+        // button was pressed.
+        //-----------------------------------------------------------------
+        public void actionPerformed (ActionEvent event)
+        {
+                JButton button = (JButton)event.getSource();
+                        JPanel buttonPanel = (JPanel)button.getParent();
+                        JPanel cardLayoutPanel = (JPanel)buttonPanel.getParent();
+                        //make new situation panel from source
+                        CardLayout layout = (CardLayout)cardLayoutPanel.getLayout();
+                        InstructionsPanel nextPanel = new InstructionsPanel(); 
+                        cardLayoutPanel.add(nextPanel,"Instructions");
+                        System.out.println("here");
+                        layout.show(cardLayoutPanel, "Instructions");
+                    }}
 
 }
--- TrailGUI.java
+++ TrailGUI.java
@@ -29,7 +29,7 @@ public class TrailGUI
         //Create the panel that contains the "cards".
         trailGame = new JPanel(new CardLayout());
         trailGame.add(start, "StartPanel");
-        
+        trailGame.setPreferredSize(new Dimension(600,400));
         pane.add(trailGame, BorderLayout.CENTER);
         
     }
