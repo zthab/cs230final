9f50919b45860138b4b5bee0521d2b79a6fd74e3
Nolen Belle <nbryant2@wellesley.edu>
2018-12-15 12:24:46 -0500

    Commented out

--- StartPanel.java
+++ StartPanel.java
@@ -2,6 +2,7 @@
 // import java.io.IOException;
 // import javax.imageio.ImageIO;
 
+<<<<<<< HEAD
 // import java.awt.*;
 // import javax.swing.*;
 // import java.awt.event.*;
@@ -19,7 +20,32 @@
     // private JLayeredPane content;//holds two panels one of which is the background image
     // private ImageIcon image;
     // private JPanel background, foreground;
+=======
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ * Creates a start panel for The Wellesley Trail
+ *
+ * @author zthabet
+ * @author gbronzi
+ * @author nbryant2
+ * @version 12.17.18
+ */
+public class StartPanel extends JPanel
+{
+    /**
+     * Constructs up a start panel as a JPanel
+     */
+    public StartPanel()
+    {
+        setBackground (Color.green);
+        setLayout(new BorderLayout());
+        JLabel l1 = new JLabel ("Welcome to Wellesley Trails!");
+        l1.setHorizontalAlignment(SwingConstants.CENTER);
+>>>>>>> 6853d8941fbb421c2caf61e8099a40a26870f14e
 
+<<<<<<< HEAD
     // /**
      // * Constructs up a JPanel with two labels.
 // =======
@@ -112,3 +138,37 @@
         // return foreground;
     // }
 // }
+=======
+        JButton next = new JButton("Next");
+        next.addActionListener(new ButtonListener());
+
+        l1.setBackground(Color.green); //so that the JTextArea matches the panel background
+        add(next,BorderLayout.SOUTH);
+        add (l1, BorderLayout.CENTER);
+
+    }
+    /**
+     * 
+     */
+    private class ButtonListener implements ActionListener
+    {
+        //-----------------------------------------------------------------
+        // Sets the text of the label depending on which radio
+        // button was pressed.
+        //-----------------------------------------------------------------
+        public void actionPerformed (ActionEvent event)
+        {
+            JButton button = (JButton)event.getSource();
+            JPanel buttonPanel = (JPanel)button.getParent();
+            JPanel cardLayoutPanel = (JPanel)buttonPanel.getParent();
+            //make new situation panel from source
+            CardLayout layout = (CardLayout)cardLayoutPanel.getLayout();
+            InstructionsPanel nextPanel = new InstructionsPanel(); 
+            cardLayoutPanel.add(nextPanel,"Instructions");
+            System.out.println("here");
+            layout.show(cardLayoutPanel, "Instructions");
+        }
+    }
+
+}
+>>>>>>> 6853d8941fbb421c2caf61e8099a40a26870f14e
