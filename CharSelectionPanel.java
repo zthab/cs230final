b14c938d24bd01819139dc7b61745209e7909f3c
Nolen Belle <nbryant2@wellesley.edu>
2018-12-15 22:34:36 -0500

    added bug message

--- CharSelectionPanel.java
+++ CharSelectionPanel.java
@@ -15,6 +15,14 @@ import javax.imageio.ImageIO;
  * to select an archetype to play as. Modifies a Person object with the 
  * archetype's sleep, smart, and social points.
  *
+ * Known bugs:
+ * When you view multiple archetypes, the old text displays over top of the new 
+ * selection for both the radio button labels and the player stats. The issue
+ * resolves itself if you resize the screen. We suspect this is related to 
+ * the background being brought into the foreground because when we moved the 
+ * foreground panel to the front it resolved the issue for one of the problem
+ * areas, but we could not find out how to solve the other.
+ * 
  * @author zthabet
  * @author gbronzi
  * @author nbryant2
